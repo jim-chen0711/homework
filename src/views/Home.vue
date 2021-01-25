c341092a921fd4c4a573865dde9533c5302edecd
jim <y77887788y@gmail.com>
2021-01-25 17:53:47 +0800

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  // created() {
+  //   // this.$http.get('/herokuapp/api/movie/list.php')
+  //   // .then((response) => {
+  //   //   console.log(response.data)
+  //   // })
+  //   // console.log("hk4g4");
+    
+  // },
+};
+</script>
